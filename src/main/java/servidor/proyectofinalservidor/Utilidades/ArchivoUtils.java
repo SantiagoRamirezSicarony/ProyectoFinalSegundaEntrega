fa4b70a0d81d8e9498a4aaa1287795cfba5e3503
SantiagoRamirezSicarony <santiago.ramirezs1@uqvirtual.edu.co>
2024-11-10 22:41:03 -0500

    Projecto con metodo para imagenes

--- src/main/java/servidor/proyectofinalservidor/Utilidades/ArchivoUtils.java
+++ src/main/java/servidor/proyectofinalservidor/Utilidades/ArchivoUtils.java
@@ -1,5 +1,11 @@
 package servidor.proyectofinalservidor.Utilidades;
 
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -157,4 +163,50 @@ public class ArchivoUtils {
             }
         });
     }
+    /**
+     * Convierte una imagen BufferedImage a un array de bytes.
+     *
+     * @param imagen La imagen a convertir.
+     * @param formato El formato de la imagen (por ejemplo, "jpg" o "png").
+     * @return Un array de bytes que representa la imagen.
+     * @throws IOException Si ocurre un error al convertir la imagen.
+     */
+    public static byte[] convertirImagenABytes(BufferedImage imagen, String formato) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(imagen, formato, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    /**
+     * Convierte un array de bytes en una imagen BufferedImage.
+     *
+     * @param bytesImagen El array de bytes que representa la imagen.
+     * @return Un BufferedImage que representa la imagen.
+     * @throws IOException Si ocurre un error al leer la imagen.
+     */
+    public static BufferedImage convertirBytesAImagen(byte[] bytesImagen) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytesImagen);
+        return ImageIO.read(byteArrayInputStream);
+    }
+
+    public Image obtenerImagen(){
+        Image image = null;
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Seleccionar imagen");
+
+        // Filtro para mostrar solo archivos de imagen
+        FileChooser.ExtensionFilter imageFilter = new FileChooser.ExtensionFilter("Archivos de imagen", "*.png", "*.jpg", "*.jpeg");
+        fileChooser.getExtensionFilters().add(imageFilter);
+
+        // Mostrar el diálogo de selección de archivo
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+
+        if (selectedFile != null) {
+            // Crear una imagen a partir del archivo seleccionado
+            image = new Image(selectedFile.toURI().toString());
+        }
+        return image;
+    }
+
+
 }
